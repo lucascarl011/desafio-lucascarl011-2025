cc66923ef853490f251c22dbae21a66409228bbd
lucascarl011 <lucassscarlosss54@gmail.com>
2025-09-08 22:56:33 -0300

    Adcionando a base de dados com os primeiros animais da lista

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,3 +1,10 @@
+// base de animais
+const ANIMAIS = {
+  Rex: { tipo: "cão", brinquedos: ["RATO", "BOLA"] },
+}
+
+
+
 class AbrigoAnimais {
 
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
