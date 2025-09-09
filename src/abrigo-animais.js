71b410917809b955a78a3acadba809b78a8e7238
lucascarl011 <lucassscarlosss54@gmail.com>
2025-09-08 23:52:24 -0300

    Refatoração e remoção dos comentários

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,17 +1,17 @@
-// base de animais
+// src/abrigo-animais.js
+
 const ANIMAIS = {
   Rex: { tipo: "cão", brinquedos: ["RATO", "BOLA"] },
   Mimi: { tipo: "gato", brinquedos: ["BOLA", "LASER"] },
   Fofo: { tipo: "gato", brinquedos: ["BOLA", "RATO", "LASER"] },
   Zero: { tipo: "gato", brinquedos: ["RATO", "BOLA"] },
-  Bola: { tipo: "cão",  brinquedo: ["CAIXA", "NOVELA"] },
-  Bebe: { tipo: "cão", brinquedo: ["LASER", "RATO", "BOLA"] },
-  Loco: { tipo: "jabuti", brinquedo: ["SKATE", "RATO"] },
+  Bola: { tipo: "cão", brinquedos: ["CAIXA", "NOVELO"] },
+  Bebe: { tipo: "cão", brinquedos: ["LASER", "RATO", "BOLA"] },
+  Loco: { tipo: "jabuti", brinquedos: ["SKATE", "RATO"] },
 };
 
-// verifica se atende animal
-function animalAceitaPessoa(brinquedosPessoa, brinquedosAnimal, ignoraOrdem = false) {
-  if (ignoraOrdem) return true;
+function pessoaAtendeAnimal(brinquedosPessoa, brinquedosAnimal, ignorarOrdem = false) {
+  if (ignorarOrdem) return true;
 
   let index = 0;
   for (const b of brinquedosPessoa) {
@@ -24,10 +24,7 @@ function animalAceitaPessoa(brinquedosPessoa, brinquedosAnimal, ignoraOrdem = fa
 }
 
 class AbrigoAnimais {
-
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
-
-    // Arrays para entradas 
     const listaBrinquedos1 = brinquedosPessoa1.split(",");
     const listaBrinquedos2 = brinquedosPessoa2.split(",");
     const animais = ordemAnimais.split(",");
@@ -40,7 +37,39 @@ class AbrigoAnimais {
       animaisSet.add(animal);
     }
 
+    const lista = [];
+    const adotadosPessoa1 = [];
+    const adotadosPessoa2 = [];
 
+    for (const animal of animais) {
+      const info = ANIMAIS[animal];
+      const ignorarOrdem = animal === "Loco";
+
+      const atende1 = pessoaAtendeAnimal(listaBrinquedos1, info.brinquedos, ignorarOrdem);
+      const atende2 = pessoaAtendeAnimal(listaBrinquedos2, info.brinquedos, ignorarOrdem);
+
+      let destino = "abrigo";
+      const limite1 = adotadosPessoa1.length < 3;
+      const limite2 = adotadosPessoa2.length < 3;
+
+      if (info.tipo === "gato") {
+        if (atende1 && atende2) destino = "abrigo";
+        else if (atende1 && limite1) destino = "pessoa 1";
+        else if (atende2 && limite2) destino = "pessoa 2";
+      } else {
+        if (atende1 && atende2) destino = "abrigo";
+        else if (atende1 && limite1) destino = "pessoa 1";
+        else if (atende2 && limite2) destino = "pessoa 2";
+      }
+
+      if (destino === "pessoa 1") adotadosPessoa1.push(animal);
+      if (destino === "pessoa 2") adotadosPessoa2.push(animal);
+
+      lista.push(`${animal} - ${destino}`);
+    }
+
+    lista.sort((a, b) => a.localeCompare(b));
+    return { erro: false, lista };
   }
 }
 
