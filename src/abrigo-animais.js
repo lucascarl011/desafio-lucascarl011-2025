130b5a04071c859342e64d6c671ef81b30961f07
lucascarl011 <lucassscarlosss54@gmail.com>
2025-09-08 23:12:53 -0300

    Adicionando função que verifica se a pessoa atende ao animal

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,7 +1,14 @@
 // base de animais
 const ANIMAIS = {
   Rex: { tipo: "cão", brinquedos: ["RATO", "BOLA"] },
-}
+  Mimi: { tipo: "gato", brinquedos: ["BOLA", "LASER"] },
+  Fofo: { tipo: "gato", brinquedos: ["BOLA", "RATO", "LASER"] },
+  Zero: { tipo: "gato", brinquedos: ["RATO", "BOLA"] },
+  Bola: { tipo: "cão",  brinquedo: ["CAIXA", "NOVELA"] },
+  Bebe: { tipo: "cão", brinquedo: ["LASER", "RATO", "BOLA"] },
+  Loco: { tipo: "jabuti", brinquedo: ["SKATE", "RATO"] },
+};
+
 
 
 
